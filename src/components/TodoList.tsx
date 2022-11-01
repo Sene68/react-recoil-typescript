00cfb6f28061f1220d4432f502770a7837717426
Sene68 <senefull68@gmail.com>
2022-11-01 20:35:30 +0900

    todo itemcreator UI 구현

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,14 @@
 import TodoHead from "./TodoHead";
+import TodoItemCreator from "./TodoItemCreator";
 
 export default function TodoList() {
     return(
         <div className="todo-template-block">
             <TodoHead />
+            <div className="todo-list-block">
+
+            </div>
+            <TodoItemCreator />
         </div>
     );
 }
